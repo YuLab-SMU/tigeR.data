9206fa36ecba9d42944447e2c6ee7c97f698b34a
Chengxugorilla <1917720764@qq.com>
2023-04-05 00:20:18 +0800

    turn to xzip file

---
+++ R/Dataset.R
@@ -0,0 +1,287 @@
+#' @title GBM_PRJNA482620_exp
+#' @description an expression matrix
+#' @name GBM_PRJNA482620_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title GBM_PRJNA482620_meta
+#' @description meta info
+#' @name GBM_PRJNA482620_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title HNSC_GSE93157_exp
+#' @description an expression matrix
+#' @name HNSC_GSE93157_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title HNSC_GSE93157_meta
+#' @description meta info
+#' @name HNSC_GSE93157_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title LGG_E_MTAB_6270_exp
+#' @description an expression matrix
+#' @name LGG_E_MTAB_6270_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title LGG_E_MTAB_6270_meta
+#' @description meta info
+#' @name LGG_E_MTAB_6270_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_GSE91061_exp
+#' @description an expression matrix
+#' @name MEL_GSE91061_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_GSE91061_meta
+#' @description meta info
+#' @name MEL_GSE91061_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_GSE93157_exp
+#' @description an expression matrix
+#' @name MEL_GSE93157_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_GSE93157_meta
+#' @description meta info
+#' @name MEL_GSE93157_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_GSE96619_exp
+#' @description an expression matrix
+#' @name MEL_GSE96619_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_GSE96619_meta
+#' @description meta info
+#' @name MEL_GSE96619_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_GSE100797_exp
+#' @description an expression matrix
+#' @name MEL_GSE100797_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_GSE100797_meta
+#' @description meta info
+#' @name MEL_GSE100797_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_GSE106128_exp
+#' @description an expression matrix
+#' @name MEL_GSE106128_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_GSE106128_meta
+#' @description meta info
+#' @name MEL_GSE106128_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_GSE115821_exp
+#' @description an expression matrix
+#' @name MEL_GSE115821_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_GSE115821_meta
+#' @description meta info
+#' @name MEL_GSE115821_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_GSE145996_exp
+#' @description an expression matrix
+#' @name MEL_GSE145996_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_GSE145996_meta
+#' @description meta info
+#' @name MEL_GSE145996_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_Nathanson_2017_exp
+#' @description an expression matrix
+#' @name MEL_Nathanson_2017_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_Nathanson_2017_meta
+#' @description meta info
+#' @name MEL_Nathanson_2017_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title MEL_phs000452_exp
+#' @description an expression matrix
+#' @name MEL_phs000452_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title MEL_phs000452_meta
+#' @description meta info
+#' @name MEL_phs000452_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title nonsqNSCLC_GSE93157_exp
+#' @description an expression matrix
+#' @name nonsqNSCLC_GSE93157_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title nonsqNSCLC_GSE93157_meta
+#' @description meta info
+#' @name nonsqNSCLC_GSE93157_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title NSCLC_GSE126044_exp
+#' @description an expression matrix
+#' @name NSCLC_GSE126044_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title NSCLC_GSE126044_meta
+#' @description meta info
+#' @name NSCLC_GSE126044_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title NSCLC_GSE135222_exp
+#' @description an expression matrix
+#' @name NSCLC_GSE135222_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title NSCLC_GSE135222_meta
+#' @description meta info
+#' @name NSCLC_GSE135222_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title RCC_Braun_2020_exp
+#' @description an expression matrix
+#' @name RCC_Braun_2020_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title RCC_Braun_2020_meta
+#' @description meta info
+#' @name RCC_Braun_2020_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title RCC_GSE67501_exp
+#' @description an expression matrix
+#' @name RCC_GSE67501_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title RCC_GSE67501_meta
+#' @description meta info
+#' @name RCC_GSE67501_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
+
+#' @title STAD_PRJEB25780_exp
+#' @description an expression matrix
+#' @name STAD_PRJEB25780_exp
+#' @docType data
+#' @format matrix
+#' @source public data set
+NULL
+
+#' @title STAD_PRJEB25780_meta
+#' @description meta info
+#' @name STAD_PRJEB25780_meta
+#' @docType data
+#' @format dataframe
+#' @source public data set
+NULL
