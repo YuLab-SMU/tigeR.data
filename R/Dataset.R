4634637947509bc72181e8a91f481699e56175d0
Chengxugorilla <1917720764@qq.com>
2023-04-17 19:59:44 +0800

    add annotation of data

--- R/Dataset.R
+++ R/Dataset.R
@@ -1,5 +1,5 @@
 #' @title GBM_PRJNA482620
-#' @description an SummarizedExperiment
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Glioblastoma patients in PRJNA482620 data set.
 #' @name GBM_PRJNA482620
 #' @docType data
 #' @format S4
@@ -7,7 +7,7 @@
 NULL
 
 #' @title HNSC_GSE93157
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Squamous head and neck patients in GSE93157 data set.
 #' @name HNSC_GSE93157
 #' @docType data
 #' @format S4
@@ -15,7 +15,7 @@ NULL
 NULL
 
 #' @title LGG_E_MTAB_6270
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Brain Lower Grade Glioma patients in E-MTAB-6270 data set.
 #' @name LGG_E_MTAB_6270
 #' @docType data
 #' @format S4
@@ -23,7 +23,7 @@ NULL
 NULL
 
 #' @title MEL_GSE91061
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in GSE91061 data set.
 #' @name MEL_GSE91061
 #' @docType data
 #' @format matrix
@@ -31,7 +31,7 @@ NULL
 NULL
 
 #' @title MEL_GSE93157
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in GSE93157 data set.
 #' @name MEL_GSE93157
 #' @docType data
 #' @format S4
@@ -39,7 +39,7 @@ NULL
 NULL
 
 #' @title MEL_GSE96619
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in GSE96619 data set.
 #' @name MEL_GSE96619
 #' @docType data
 #' @format S4
@@ -47,7 +47,7 @@ NULL
 NULL
 
 #' @title MEL_GSE100797
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in GSE100797 data set.
 #' @name MEL_GSE100797
 #' @docType data
 #' @format S4
@@ -55,7 +55,7 @@ NULL
 NULL
 
 #' @title MEL_GSE106128
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in GSE106128 data set.
 #' @name MEL_GSE106128
 #' @docType data
 #' @format S4
@@ -63,7 +63,7 @@ NULL
 NULL
 
 #' @title MEL_GSE115821
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in GSE115821 data set.
 #' @name MEL_GSE115821
 #' @docType data
 #' @format S4
@@ -71,7 +71,7 @@ NULL
 NULL
 
 #' @title MEL_GSE145996
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in GSE145996 data set.
 #' @name MEL_GSE145996
 #' @docType data
 #' @format S4
@@ -79,7 +79,7 @@ NULL
 NULL
 
 #' @title MEL_Nathanson_2017
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in Nathanson_2017 data set.
 #' @name MEL_Nathanson_2017
 #' @docType data
 #' @format S4
@@ -87,7 +87,7 @@ NULL
 NULL
 
 #' @title MEL_phs000452
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Melanoma patients in phs000452 data set.
 #' @name MEL_phs000452
 #' @docType data
 #' @format S4
@@ -95,7 +95,7 @@ NULL
 NULL
 
 #' @title nonsqNSCLC_GSE93157
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Nonsmall-cell lung cancer patients in GSE93157 data set.
 #' @name nonsqNSCLC_GSE93157
 #' @docType data
 #' @format S4
@@ -103,7 +103,7 @@ NULL
 NULL
 
 #' @title NSCLC_GSE126044
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Nonsmall-cell lung cancer patients in GSE126044 data set.
 #' @name NSCLC_GSE126044
 #' @docType data
 #' @format S4
@@ -111,7 +111,7 @@ NULL
 NULL
 
 #' @title NSCLC_GSE135222
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Nonsmall-cell lung cancer patients in GSE135222 data set.
 #' @name NSCLC_GSE135222
 #' @docType data
 #' @format S4
@@ -119,7 +119,7 @@ NULL
 NULL
 
 #' @title RCC_Braun_2020
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Renal cell carcinoma patients in Braun_2020 data set.
 #' @name RCC_Braun_2020
 #' @docType data
 #' @format S4
@@ -127,7 +127,7 @@ NULL
 NULL
 
 #' @title RCC_GSE67501
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Renal cell carcinoma patients in GSE67501 data set.
 #' @name RCC_GSE67501
 #' @docType data
 #' @format S4
@@ -135,7 +135,7 @@ NULL
 NULL
 
 #' @title STAD_PRJEB25780
-#' @description an expression matrix
+#' @description An SummarizedExperiment object contains expression matrix and clinical information of Stomach adenocarcinoma patients in PRJEB25780 data set.
 #' @name STAD_PRJEB25780
 #' @docType data
 #' @format S4
